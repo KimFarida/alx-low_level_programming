84f37ebe35ac0b53e92a49648ce07e20573555e2
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-06-19 00:27:36 +0100

    Betty-style 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,6 +5,7 @@
 * whose values do not exceed 4,000,000,
 * write a program that finds and prints the sum of the even-valued terms,
 * followed by a new line.
+* Return: Always 0.
 */
 
 int main(void)
