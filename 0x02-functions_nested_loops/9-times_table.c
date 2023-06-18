1785303c2fd17ef3a55ccb7ea43753ba2ef10f0a
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-06-18 14:27:39 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			_putchar(' '); /* Print a space before each element */
 	
 	
-			if (answer <= 9) /* If the answer is a single digit */
+			if (answer <= 9 && answer != 0) /* If the answer is a single digit */
 			{
 				_putchar(' '); /* Print an additional space for alignment */
 				_putchar('0' + answer);
