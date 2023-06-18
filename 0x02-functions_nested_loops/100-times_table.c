2bc825c70b61f7ec8593164d16c673c8bb9e3144
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-06-18 17:59:26 +0100

    Added comments

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,33 +9,40 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
+	
+	/* Check if the given number is within the valid range */
 	if (n > 15 || n < 0)
 	return;
 
+	/* Loop through rows of the table */
 	for (i = 0; i <= n; i++)
 		{
-		for (j = 0; j <= n; j++)
-		{
-			k = i * j;
-			if (k <= 9 && j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				printf(" ");
-				printf(" ");
+				k = i * j; /* Compute the multiplication */
+
+				/* Add spaces for alignment based on the result */
+				if (k <= 9 && j != 0)
+				{
+					printf(" ");
+					printf(" ");
+				}
+				else if (k >= 10 && k < 100)
+				{
+					printf(" ");
+				}
+				/* Add a space before the result if it's not the first column */
+				if (j != 0)
+					printf(" ");
+				printf("%d", k);
+				/* Add a comma to separate elements if it's not the last column */
+				if (j != n)
+				{
+					printf(",");
+				}
+				k++;
 			}
-			else if (k >= 10 && k < 100)
-			{
-				printf(" ");
-			}
-			if (j != 0)
-				printf(" ");
-			printf("%d", k);
-			if (j != n)
-			{
-				printf(",");
-			}
-			k++;
-		}
+		/* Move to the next line for a new row */
 		printf("\n");
 
 	}
