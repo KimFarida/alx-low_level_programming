1a65df63fe91c9c6c72b144a5968d3ca35699838
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-06-19 00:25:08 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
 * main - entry point
-* Description - By considering the terms in the Fibonacci sequence
+* Description: By considering the terms in the Fibonacci sequence
 * whose values do not exceed 4,000,000,
 * write a program that finds and prints the sum of the even-valued terms,
 * followed by a new line.
