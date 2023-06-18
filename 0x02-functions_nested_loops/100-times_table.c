4d9fe02aaa4b879f2ad5ffe89ce35f8e599cba7c
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-06-18 18:00:56 +0100

    Betty code styling

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
+
 	/* Check if the given number is within the valid range */
 	if (n > 15 || n < 0)
 	return;
@@ -17,31 +17,31 @@ void print_times_table(int n)
 	/* Loop through rows of the table */
 	for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = i * j; /* Compute the multiplication */
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j; /* Compute the multiplication */
 
-				/* Add spaces for alignment based on the result */
-				if (k <= 9 && j != 0)
-				{
-					printf(" ");
-					printf(" ");
-				}
-				else if (k >= 10 && k < 100)
-				{
-					printf(" ");
-				}
-				/* Add a space before the result if it's not the first column */
-				if (j != 0)
-					printf(" ");
-				printf("%d", k);
-				/* Add a comma to separate elements if it's not the last column */
-				if (j != n)
-				{
-					printf(",");
-				}
-				k++;
+			/* Add spaces for alignment based on the result */
+			if (k <= 9 && j != 0)
+			{
+				printf(" ");
+				printf(" ");
 			}
+			else if (k >= 10 && k < 100)
+			{
+				printf(" ");
+			}
+			/* Add a space before the result if it's not the first column */
+			if (j != 0)
+				printf(" ");
+			printf("%d", k);
+			/* Add a comma to separate elements if it's not the last column */
+			if (j != n)
+			{
+				printf(",");
+			}
+			k++;
+		}
 		/* Move to the next line for a new row */
 		printf("\n");
 
