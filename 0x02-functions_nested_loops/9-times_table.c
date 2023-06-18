883dee61aa9a5dece3c378fd17feddb3bb89c014
KimFarida <95109808+KimFarida@users.noreply.github.com>
2023-06-18 15:18:34 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,43 +7,40 @@
 
 void times_table(void)
 {
-	int i, j, k, answer;
-	
-	for (i = 0; i <= 9; i++) /* Outer loop for rows */
-	{
-		answer = 0;
-		k = 0;
-		for (j = 0; j <= 9; j++) /*Loop acts as a counter*/
-		{
-			answer = i * k;
 
-			if(j != 0)
-				_putchar(' '); /* Print a space before each element */
-	
-	
-			if (answer <= 9) /* If the answer is a single digit */
-			{
-				_putchar(' '); /* Print an additional space for alignment */
-				_putchar(answer + '0');
-			}
-			else
-			{
-				_putchar((answer / 10) + '0');
-				_putchar((answer % 10) + '0');
-				
-			}
-			
-			
-	
-			if (j != 9) /* If it's not the last column */
-			{
-				_putchar(','); /* Print a comma to separate elements */
-	
-			}
-			k++;
+int a = 0;
+int b;
+int rep;
+while (a <= 9)
+{
+	b = 0;
+	while (b <= 9)
+	{
+		rep = a * b;
+		if (b == 0)
+		{
+			_putchar('0' + rep);
 		}
-		if(i != 9)
-			_putchar('\n'); /* Move to the next line for a new row */
-			_putchar('\0'); /* Add an extra null character */
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
 	}
+	_putchar('\n');
+	a++;
+}
+
 }
